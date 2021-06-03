47593235bf69a7c470a10b784c641ca59191c789
rzoktan <rz.oktan@gmail.com>
2021-06-03 09:11:50 +0700

    Add session check on controller

--- application/controllers/user/Dashboard.php
+++ application/controllers/user/Dashboard.php
@@ -15,6 +15,10 @@ class Dashboard extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if ($this->session->has_userdata('username') == null) {
+            $this->session->set_flashdata('message', "<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <h4><i class='icon fa fa-warning'></i> Alert!</h4> Harus Login Terlebih Dahulu</div>");
+            redirect(base_url());
+        }
         $this->load->model('M_menu', 'menu');
         $this->load->model('M_user', 'user');
     }
--- application/controllers/user/Transaksi.php
+++ application/controllers/user/Transaksi.php
@@ -13,6 +13,10 @@ class Transaksi extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if ($this->session->has_userdata('username') == null) {
+            $this->session->set_flashdata('message', "<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <h4><i class='icon fa fa-warning'></i> Alert!</h4> Harus Login Terlebih Dahulu</div>");
+            redirect(base_url());
+        }
     }
     public function index()
     {
