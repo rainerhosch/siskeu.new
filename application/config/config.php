f61077b0420a7808e0b9d066cd2979dcf3fbb8c4
rz.oktan <rz.oktan@gmail.com>
2022-09-09 17:49:04 +0700

    update session time expired

--- application/config/config.php
+++ application/config/config.php
@@ -386,7 +386,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 0;
+$config['sess_expiration'] = 1;
 // $config['sess_save_path'] = NULL;
 $config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
