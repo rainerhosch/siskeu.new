cdd4f3fffd32796eb838d52791080318fe78505f
rz.oktan <rz.oktan@gmail.com>
2023-08-10 15:58:54 +0700

    update

--- application/controllers/user/MigrasiTrxToTg.php
+++ application/controllers/user/MigrasiTrxToTg.php
@@ -11,15 +11,38 @@ defined('BASEPATH') or exit('No direct script access allowed');
  */
 class MigrasiTrxToTg extends CI_Controller
 {
+    private $smt_aktif;
     public function __construct()
     {
         parent::__construct();
+        if ($this->session->has_userdata('username') == null) {
+            $this->session->set_flashdata('message', "<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <h4><i class='icon fa fa-warning'></i> Alert!</h4> Harus Login Terlebih Dahulu</div>");
+            redirect(base_url());
+        }
+        date_default_timezone_set('Asia/Jakarta');
+        $this->load->library('Terbilang');
+        $this->load->library('FormatTanggal');
+
+
+        $this->load->model('M_cetak_kwitansi', 'cetak');
+        $this->load->model('M_masterdata', 'masterdata');
+        $this->load->model('M_transaksi', 'transaksi');
+        $this->load->model('M_tunggakan', 'tunggakan');
+        $this->load->model('M_aktivasi_mhs', 'aktivasi');
+        $this->load->model('M_user', 'user');
     }
 
     public function index()
     {
         $data['title'] = 'Utility';
-        $data['content'] = 'layout/my_error';
+        $data['page'] = 'Test Page';
+        $data['content'] = 'layout/test_page';
         $this->load->view('template', $data);
+    }
+
+    public function migrate_to_tg()
+    {
+        $smtAktifRes = $this->masterdata->getSemesterAktif()->row_array();
+        $smtAktif = $smtAktifRes['id_smt'];
     }
 }
