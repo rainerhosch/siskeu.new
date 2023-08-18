e2092ef174a78dbc1aca7dcce2bcc94d5d4ccb15
rz.oktan <rz.oktan@gmail.com>
2023-08-18 11:28:22 +0700

    xxx

--- application/controllers/user/MigrasiTrxToTg.php
+++ application/controllers/user/MigrasiTrxToTg.php
@@ -149,7 +149,7 @@ class MigrasiTrxToTg extends CI_Controller
         $data['total_lunas'] = 0;
         $data['total_belum_lunas'] = 0;
         foreach ($dataHistoriTxByNim as $i => $val) {
-            $data[$i]['bayar_lainnya'] = true;
+            // $data[$i]['bayar_lainnya'] = true;
             $data[$i]['status_pembayaran'] = 'Belum Lunas';
             $data[$i]['total_bayar_cs'] = 0;
             $data[$i]['total_bayar_ps'] = 0;
@@ -181,9 +181,9 @@ class MigrasiTrxToTg extends CI_Controller
                 $dataDetailTrx = $this->transaksi->getDataDetailTransaksiOnly(['id_transaksi' => $trx['id_transaksi']])->result_array();
                 $data[$i]['histori_trx'][$j]['detail_trx'] = $dataDetailTrx;
                 foreach ($dataDetailTrx as $x => $dtx) {
-                    if ($dtx['id_jenis_pembayaran'] != '2' || $dtx['id_jenis_pembayaran'] != '3' || $dtx['id_jenis_pembayaran'] != '4') {
-                        $data[$i]['bayar_lainya'] = true;
-                    }
+                    // if ($dtx['id_jenis_pembayaran'] != '2' || $dtx['id_jenis_pembayaran'] != '3' || $dtx['id_jenis_pembayaran'] != '4') {
+                    //     $data[$i]['bayar_lainya'] = true;
+                    // }
                     if ($dtx['id_jenis_pembayaran'] === '2' || $dtx['id_jenis_pembayaran'] === '3' || $dtx['id_jenis_pembayaran'] === '4') {
                         $data[$i]['total_bayar_cs'] = $data[$i]['total_bayar_cs'] + $dtx['jml_bayar'];
                         $data[$i]['histori_trx'][$j]['detail_trx'][$x]['sisa_bayar_cs'] = $data[$i]['kewajiban_cs'] - $data[$i]['total_bayar_cs'];
@@ -201,14 +201,13 @@ class MigrasiTrxToTg extends CI_Controller
                 }
 
             }
-            if ($data[$i]['bayar_lainya'] === false) {
-                if ($data[$i]['kewajiban_cs'] - $data[$i]['total_bayar_cs'] <= 0) {
-                    $data[$i]['status_pembayaran'] = 'Lunas';
-                    $data['total_lunas'] = $data['total_lunas'] + 1;
-                }
-                if ($data[$i]['kewajiban_cs'] - $data[$i]['total_bayar_cs'] > 0) {
-                    $data['total_belum_lunas'] = $data['total_belum_lunas'] + 1;
-                }
+
+            if ($data[$i]['kewajiban_cs'] - $data[$i]['total_bayar_cs'] <= 0) {
+                $data[$i]['status_pembayaran'] = 'Lunas';
+                $data['total_lunas'] = $data['total_lunas'] + 1;
+            }
+            if ($data[$i]['kewajiban_cs'] - $data[$i]['total_bayar_cs'] > 0) {
+                $data['total_belum_lunas'] = $data['total_belum_lunas'] + 1;
             }
 
             // $dataDetailTrx = $this->transaksi->getDataDetailTransaksiOnly(['id_transaksi' => $val['id_transaksi']])->result_array();
