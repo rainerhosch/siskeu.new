d4d12b7d242e21162d0f23ab1f612654aae41b34
rz.oktan <rz.oktan@gmail.com>
2023-08-24 16:33:18 +0700

    update

--- application/views/transaksi/pembayaran.php
+++ application/views/transaksi/pembayaran.php
@@ -94,62 +94,62 @@
         <?= $this->session->flashdata('message'); ?>
     </div>
     <?php if ($this->session->userdata('role') != 4): ?>
-                                                                    <div class="row">
-                                                                        <div class="col-sm-6 col-lg-3">
-                                                                            <a href="#pembayaran-spp" class="widget widget-hover-effect1" data-toggle="modal" data-target="#formPembayaran">
-                                                                                <div class="widget-simple">
-                                                                                    <div class="widget-icon pull-left themed-background-spring animation-fadeIn">
-                                                                                        <i class="fa fa-bitcoin"></i>
+                                                                        <div class="row">
+                                                                            <div class="col-sm-6 col-lg-3">
+                                                                                <a href="#pembayaran-spp" class="widget widget-hover-effect1" data-toggle="modal" data-target="#formPembayaran">
+                                                                                    <div class="widget-simple">
+                                                                                        <div class="widget-icon pull-left themed-background-spring animation-fadeIn">
+                                                                                            <i class="fa fa-bitcoin"></i>
+                                                                                        </div>
+                                                                                        <h3 class="widget-content text-right animation-pullDown">
+                                                                                            Pembayaran <strong>SPP</strong><br>
+                                                                                            <small>Buat Transaksi Baru</small>
+                                                                                        </h3>
                                                                                     </div>
-                                                                                    <h3 class="widget-content text-right animation-pullDown">
-                                                                                        Pembayaran <strong>SPP</strong><br>
-                                                                                        <small>Buat Transaksi Baru</small>
-                                                                                    </h3>
-                                                                                </div>
-                                                                            </a>
-                                                                        </div>
-                                                                        <div class="col-sm-6 col-lg-3">
-                                                                            <a href="#pembayaran-lain" class="widget widget-hover-effect1" data-toggle="modal" data-target="#formPembayaranLain">
-                                                                                <div class="widget-simple">
-                                                                                    <div class="widget-icon pull-left themed-background-autumn animation-fadeIn">
-                                                                                        <i class="fa fa-dollar"></i>
+                                                                                </a>
+                                                                            </div>
+                                                                            <div class="col-sm-6 col-lg-3">
+                                                                                <a href="#pembayaran-lain" class="widget widget-hover-effect1" data-toggle="modal" data-target="#formPembayaranLain">
+                                                                                    <div class="widget-simple">
+                                                                                        <div class="widget-icon pull-left themed-background-autumn animation-fadeIn">
+                                                                                            <i class="fa fa-dollar"></i>
+                                                                                        </div>
+                                                                                        <h3 class="widget-content text-right animation-pullDown">
+                                                                                            Pembayaran <strong>Lain</strong><br>
+                                                                                            <small>Buat Transaksi Baru</small>
+                                                                                        </h3>
                                                                                     </div>
-                                                                                    <h3 class="widget-content text-right animation-pullDown">
-                                                                                        Pembayaran <strong>Lain</strong><br>
-                                                                                        <small>Buat Transaksi Baru</small>
-                                                                                    </h3>
-                                                                                </div>
-                                                                            </a>
-                                                                        </div>
-                                                                        <div class="col-sm-6 col-lg-3 btn_trf_online">
-                                                                            <a href="#data-transfer" class="widget widget-hover-effect1">
-                                                                                <!-- <a href="#data-transfer" class="widget widget-hover-effect1" data-toggle="modal" data-target="#dataTransferMhs"> -->
-                                                                                <div class="widget-simple">
-                                                                                    <div class="widget-icon pull-left themed-background-amethyst animation-fadeIn">
-                                                                                        <i class="gi gi-circle_info"></i>
+                                                                                </a>
+                                                                            </div>
+                                                                            <div class="col-sm-6 col-lg-3 btn_trf_online">
+                                                                                <a href="#data-transfer" class="widget widget-hover-effect1">
+                                                                                    <!-- <a href="#data-transfer" class="widget widget-hover-effect1" data-toggle="modal" data-target="#dataTransferMhs"> -->
+                                                                                    <div class="widget-simple">
+                                                                                        <div class="widget-icon pull-left themed-background-amethyst animation-fadeIn">
+                                                                                            <i class="gi gi-circle_info"></i>
+                                                                                        </div>
+                                                                                        <h3 class="widget-content text-right animation-pullDown">
+                                                                                            <strong id="label-jmlpembayaran">Pembayaran</strong><br>
+                                                                                            <small>Mahasiswa Transfer</small>
+                                                                                        </h3>
                                                                                     </div>
-                                                                                    <h3 class="widget-content text-right animation-pullDown">
-                                                                                        <strong id="label-jmlpembayaran">Pembayaran</strong><br>
-                                                                                        <small>Mahasiswa Transfer</small>
-                                                                                    </h3>
-                                                                                </div>
-                                                                            </a>
-                                                                        </div>
-                                                                        <div class="col-sm-6 col-lg-3">
-                                                                            <a href="#" class="widget widget-hover-effect1">
-                                                                                <div class="widget-simple">
-                                                                                    <div class="widget-icon pull-left themed-background animation-fadeIn">
-                                                                                        <!-- <i class="gi gi-wallet"></i> -->
-                                                                                        <i class="fa fa-file-text-o"></i>
+                                                                                </a>
+                                                                            </div>
+                                                                            <div class="col-sm-6 col-lg-3">
+                                                                                <a href="#" class="widget widget-hover-effect1">
+                                                                                    <div class="widget-simple">
+                                                                                        <div class="widget-icon pull-left themed-background animation-fadeIn">
+                                                                                            <!-- <i class="gi gi-wallet"></i> -->
+                                                                                            <i class="fa fa-file-text-o"></i>
+                                                                                        </div>
+                                                                                        <h3 class="widget-content text-right animation-pullDown">
+                                                                                            <?= $jumlah_tx_hari_ini; ?> <strong>Transaksi</strong><br>
+                                                                                            <small>Transaksi Hari Ini</small>
+                                                                                        </h3>
                                                                                     </div>
-                                                                                    <h3 class="widget-content text-right animation-pullDown">
-                                                                                        <?= $jumlah_tx_hari_ini; ?> <strong>Transaksi</strong><br>
-                                                                                        <small>Transaksi Hari Ini</small>
-                                                                                    </h3>
-                                                                                </div>
-                                                                            </a>
+                                                                                </a>
+                                                                            </div>
                                                                         </div>
-                                                                    </div>
     <?php endif; ?>
 
     <div class="row">
@@ -412,7 +412,7 @@
                         }
 
                         if (value.total_bayar < value.kewajiban_bayar) {
-                            htmlx += `<td class = "text-center text-danger" >BL</td>`;
+                            htmlx += `<td class = "text-center text-danger" ><i style="font-size:1.2rem; font-weight: bold;">Belum Lunas</i></td>`;
                         } else {
                             htmlx += `<td class = "text-center text-success" ><i style="font-size:1.2rem; font-weight: bold;">Lunas</i></td>`;
                         }
