a1f7ecfdecc6c69842823bee6d228c9d5cfb47b4
Rainer Hosch <54917867+rainerhosch@users.noreply.github.com>
2024-03-07 15:48:42 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -2,7 +2,6 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
-TEST
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
 | -------------------------------------------------------------------
