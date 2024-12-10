4c35930a3844489355e6f13fa3f2aed24b9ccbda
rz.oktan <rz.oktan@gmail.com>
2024-12-10 12:37:22 +0700

    update : add cors middleware

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['post_controller_constructor'][] = array(
+    'class'    => 'CorsMiddleware',
+    'function' => 'handle',
+    'filename' => 'CorsMiddleware.php',
+    'filepath' => 'hooks'
+);
+
---
+++ application/hooks/CorsMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CorsMiddleware {
+    public function handle() {
+        header("Access-Control-Allow-Origin: https://siskeu.wastu.digital");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
+        header("Access-Control-Allow-Credentials: true");
+
+        // Cek request OPTIONS
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            exit(0);
+        }
+    }
+}
