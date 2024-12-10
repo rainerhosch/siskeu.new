5af312ff14154052bb672a230b5f0c991ffa2b64
rz.oktan <rz.oktan@gmail.com>
2024-12-10 12:40:23 +0700

    update lagi :(

--- application/hooks/CorsMiddleware.php
+++ application/hooks/CorsMiddleware.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CorsMiddleware {
     public function handle() {
-        header("Access-Control-Allow-Origin: https://siskeu.wastu.digital");
+        header("Access-Control-Allow-Origin: siskeu.wastu.digital");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
         header("Access-Control-Allow-Credentials: true");
