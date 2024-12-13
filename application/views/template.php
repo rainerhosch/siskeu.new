526b02d4511be04a2b0111306dc6d09a1ca276b2
rz.oktan <rz.oktan@gmail.com>
2024-12-13 13:51:10 +0700

    update: fix performance

--- application/views/template.php
+++ application/views/template.php
@@ -11,15 +11,15 @@
     <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">
 
-    <link rel="shortcut icon" href="<?= base_url() ?>assets/proui/img/favicon/logo.png" disabled mdia="all">
-    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" type="text/css" disabled mdia="all">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/bootstrap.min.css" disabled mdia="all">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/plugins.css" disabled mdia="all">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/main.css" disabled mdia="all">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/themes.css" disabled mdia="all">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/template/css/my.css" disabled mdia="all">
+    <link rel="shortcut icon" href="<?= base_url() ?>assets/proui/img/favicon/logo.png" media="all">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/bootstrap.min.css" media="all">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/plugins.css" media="all">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/main.css" media="all">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/proui/css/themes.css" media="all">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template/css/my.css" media="all">
     <!-- <link rel="stylesheet" href="<?= base_url() ?>assets/proui/swal/dist/sweetalert2.min.css"> -->
-    <link rel="stylesheet" href="<?= base_url('assets/template/sweetalert2/dist/sweetalert2.min.css') ?>" disabled mdia="all">
+    <link rel="stylesheet" href="<?= base_url('assets/template/sweetalert2/dist/sweetalert2.min.css') ?>" media="all">
 
     <!-- javascript -->
     <script src="<?= base_url() ?>assets/proui/js/vendor/modernizr.min.js" async></script>
