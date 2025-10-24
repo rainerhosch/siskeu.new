db0e6e6334c3f26a3f403819d9a743516373658d
rizkyoktan <rizkyoktan@wastukancana.ac.id>
2025-10-24 22:10:30 +0700

    fix: comment out redundant payment adjustment logic in transaction processing to improve clarity

--- application/controllers/user/Transaksi.php
+++ application/controllers/user/Transaksi.php
@@ -2385,11 +2385,11 @@ class Transaksi extends CI_Controller
                         $resDetailTx[$x]['kewajiban_Bayar'] = $kewajibanPerpanjangSemester;
                     } else if ($resDetailTx[$x]['id_jenis_pembayaran'] == 9) {
                         $resDetailTx[$x]['id_jenis_pembayaran'] == $dtxs['id_jenis_pembayaran'];
-                        foreach ($dataTxSebelumnya as $jj => $dtxs){
-                            if($dtxs['id_jenis_pembayaran'] == 9){
-                                $resDetailTx[$x]['kewajiban_Bayar'] = $resDetailTx[$x]['kewajiban_Bayar'] - $dtxs['jml_bayar'];
-                            }
-                        }
+                        // foreach ($dataTxSebelumnya as $jj => $dtxs){
+                        //     if($dtxs['id_jenis_pembayaran'] == 9){
+                        //         $resDetailTx[$x]['kewajiban_Bayar'] = $resDetailTx[$x]['kewajiban_Bayar'] - $dtxs['jml_bayar'];
+                        //     }
+                        // }
                         // if (count($dataTxSebelumnya) > 0) {
                         //     if ($resDetailTx[$x]['id_jenis_pembayaran'] == $dataTxSebelumnya[$x]['id_jenis_pembayaran']) {
                         //         $resDetailTx[$x]['kewajiban_Bayar'] = $dataBiayaAngkatan['uang_bangunan'] - $dataTxSebelumnya[$x]['jml_bayar'];
@@ -2962,11 +2962,11 @@ class Transaksi extends CI_Controller
                         $resDetailTx[$x]['kewajiban_Bayar'] = $kewajibanPerpanjangSemester;
                     } else if ($resDetailTx[$x]['id_jenis_pembayaran'] == 9) {
                         $resDetailTx[$x]['id_jenis_pembayaran'] == $dtxs['id_jenis_pembayaran'];
-                        foreach ($dataTxSebelumnya as $jj => $dtxs){
-                            if($dtxs['id_jenis_pembayaran'] == 9){
-                                $resDetailTx[$x]['kewajiban_Bayar'] = $resDetailTx[$x]['kewajiban_Bayar'] - $dtxs['jml_bayar'];
-                            }
-                        }
+                        // foreach ($dataTxSebelumnya as $jj => $dtxs){
+                        //     if($dtxs['id_jenis_pembayaran'] == 9){
+                        //         $resDetailTx[$x]['kewajiban_Bayar'] = $resDetailTx[$x]['kewajiban_Bayar'] - $dtxs['jml_bayar'];
+                        //     }
+                        // }
                         // if (count($dataTxSebelumnya) > 0) {
                         //     foreach ($dataTxSebelumnya as $jj => $dtxs) {
                         //         $resDetailTx[$x]['id_jenis_pembayaran'] == $dtxs['id_jenis_pembayaran'] ? $resDetailTx[$x]['kewajiban_Bayar'] = $dataBiayaAngkatan['uang_bangunan'] - $dtxs['jml_bayar'] : $resDetailTx[$x]['kewajiban_Bayar'] = $dataBiayaAngkatan['uang_bangunan'];
